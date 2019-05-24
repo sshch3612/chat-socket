fb57f16bfc11484721bd40996893addb8b9f9f3e
sshch3612 <1758974924@qq.com>
2019-05-24 22:13:53 +0800

    修改文件上传函数

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -6,8 +6,9 @@ const request = require("request");
 const Uuid = require("uuid");
 const qiniu = require("qiniu");
 
-const accessKey = "bcIae8Et0Lf6kzuU5lFk-vud4TW8sIMqMliPHJ7I";
-const secretKey = "ms7pwXuCoiJOG6nvQfszZQM3OwMutOjpvcRigh2o";
+
+const accessKey = "bcIae8Et0Lf6kzuU5lFk-vud4TW8s22222233443";
+const secretKey = "ms7pwXuCoiJOG6nvQfszZQM3OwMu998833232344";
 const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
 //要上传的空间
 const options = {
@@ -30,23 +31,26 @@ module.exports = {
     console.log("测试一下");
   },
   //图片上传7牛云 base64
-  async fileUpload(urlpath, data) {
+  async fileUpload(urlpath, data, filename) {
     const datainfo = data.replace(`${data.split(",")[0]},`, "");
     const baseUrl = "http://pry40zxwo.bkt.clouddn.com/";
+    filename = filename || Uuid.v1();
     return new Promise((resolve, reject) => {
       request(
         {
-          url: `http://upload-z2.qiniu.com/putb64/-1${urlpath}/${Uuid.v1()}`, //请求路径
+          url: `http://upload-z2.qiniu.com/putb64/-1${urlpath}/${filename}`, //请求路径
           method: "POST", //请求方式，默认为get
           headers: {
             //设置请求头
             "content-type": "application/octet-stream",
-            Authorization: `UpToken ${uploadToken}`
+            Authorization: `UpToken ${uploadToken}`,
+            key: filename
           },
           body: datainfo //JSON.stringify(requestData) //post参数字符串
         },
         function(error, response, body) {
           if (!error && response.statusCode == 200) {
+            console.log(filename, body, 999);
             resolve(`${baseUrl}${JSON.parse(body).key}`);
           }
         }
